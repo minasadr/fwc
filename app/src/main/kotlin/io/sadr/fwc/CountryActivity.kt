a6fea4de789fa3e6f3cc06b10baeca494de7efb8
mina <mina.sh.sadr@gmail.com>
2016-09-04 20:01:29 +0200

    fullscrean feature

--- app/src/main/kotlin/io/sadr/fwc/CountryActivity.kt
+++ app/src/main/kotlin/io/sadr/fwc/CountryActivity.kt
@@ -6,6 +6,7 @@ import android.app.FragmentManager
 import android.os.Bundle
 import android.support.v13.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
+import android.view.View
 import java.util.*
 
 class CountryActivity : Activity() {
@@ -37,6 +38,19 @@ class CountryActivity : Activity() {
                 }
             }
         })
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            val viewPager = findViewById(R.id.pager) as ViewPager
+            viewPager.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
     }
 
     private inner class PagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
